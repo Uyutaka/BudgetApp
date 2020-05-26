30c8fc16df7ca47b7cebcd10a83135f1fb0fc986
Yutaka Urakami <Yutaka.Urakami@coloraod.edu>
2020-05-26 06:33:28 -0600

    Setup test db env

---
+++ src/firebase/firebase.js
@@ -0,0 +1,50 @@
+import * as firebase from 'firebase';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
+const database = firebase.database();
+
+export {firebase,database as default };
+// database.ref('expenses').push({
+//     description: 'Rent',
+//     note: 'First',
+//     amount: 345,
+//     createdAt: 345342
+// })
+// database.ref('expenses').push({
+//     description: 'Food',
+//     note: 'Second',
+//     amount: 34500,
+//     createdAt: 345342
+// })
+// database.ref('expenses').push({
+//     description: 'Utility Bill',
+//     note: 'Third',
+//     amount: 345000,
+//     createdAt: 345342
+// })
+
+// database.ref('expenses').on('value',(snapshot)=>{
+//     const expenses = [];
+//     snapshot.forEach((childSnapshot)=>{
+//         expenses.push({
+//             id: childSnapshot.key,
+//             ...childSnapshot.val()
+//         })
+//     })
+//     console.log(expenses);
+// })
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,16 +1,23 @@
 // Store creation
-import {combineReducers, createStore} from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import expensesReducer from '../reducers/expenses'
 import filtersReducer from '../reducers/filters'
+import thunk from 'redux-thunk';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() || compose;
 export default () => {
     const store = createStore(
         combineReducers({
             expenses: expensesReducer,
             filters: filtersReducer
         }),
+        undefined,
+        compose(
+            applyMiddleware(thunk),
+            window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : noop => noop,
+        )
         // __REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     );
     return store;
-}
+};
