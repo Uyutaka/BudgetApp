3dd66fa2e778f5fcf7062fccace8ada99c998c37
Yutaka Urakami <Yutaka.Urakami@coloraod.edu>
2020-05-27 06:39:21 -0600

    Get firebase expenses on start

--- src/app.js
+++ src/app.js
@@ -3,25 +3,16 @@ import ReactDOM from 'react-dom';
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
 import {Provider} from 'react-redux';
-import {addExpense} from "./actions/expenses";
-import getVisibleExpenses from './selectors/expenses'
+import {startSetExpenses} from "./actions/expenses";
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css'
 import 'react-dates/initialize';
 import './firebase/firebase';
-// import './playground/promises';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'water bill', amount: 4500}));
-store.dispatch(addExpense({description: 'gas bill', createdAt: 1000}));
-store.dispatch(addExpense({description: 'Rent', amount: 109500}));
 
-
-const state = store.getState()
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-// console.log(visibleExpenses);
 
 
 const jsx = (
@@ -29,7 +20,11 @@ const jsx = (
         <AppRouter/>
     </Provider>
 );
-
 ReactDOM.render(
-    jsx,
+    <p>Loading</p>,
     document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(()=>{
+    ReactDOM.render(jsx, document.getElementById('app'));
+})
+
